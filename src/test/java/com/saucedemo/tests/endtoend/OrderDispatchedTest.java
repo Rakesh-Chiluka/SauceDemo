11bd6076d0f631608c35253ee07a197d7b930448
Rakesh-Chiluka <rakesh.chiluka999@gmail.com>
2023-09-14 20:30:40 +0530

    before revering the revert

---
+++ src/test/java/com/saucedemo/tests/endtoend/OrderDispatchedTest.java
@@ -0,0 +1,16 @@
+package com.saucedemo.tests.endtoend;
+
+
+import org.testng.annotations.Test;
+
+import com.saucedemo.utils.CommonUtils;
+import com.saucedemo.utils.ExtentUtils;
+
+public class OrderDispatchedTest extends CommonUtils {
+	
+	@Test
+	public void OrderDispatchedJourney() {
+		System.out.println("Order dispatched");
+	}
+
+}
